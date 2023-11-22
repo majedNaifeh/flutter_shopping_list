4c9d5eca04b8855c5e572d835514794f3a2811d3
majedNaifeh <majed.naifeh@outlook.com>
2023-11-22 19:13:52 +0200

    Initial commit

---
+++ lib/models/category.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+enum Categories {
+  vegetables,
+  fruit,
+  meat,
+  dairy,
+  carbs,
+  sweets,
+  spices,
+  convenience,
+  hygiene,
+  other,
+}
+
+class Category {
+  const Category(this.name, this.color);
+
+  //final Categories categories;
+  final String name;
+  final Color color;
+}
